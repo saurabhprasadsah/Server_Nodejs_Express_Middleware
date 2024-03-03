2bb193938b28c8d673fe4167a898780033ca40a6
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-03-03 19:40:38 +0530

    testingapi

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
+
 const express = require("express")
 const users = require('./MOCK_DATA.json')
 const app = express()
 const PORT = 8000;
 
 
-// app.get('/users',(req,res) =>{
-//     const html =`
-//     <ul>
-//          ${users.map((user) => `<li>${user.first_name}</li>`).join("  ")}
-//     </ul>
-//     `;
-//     res.send(html)
+app.get('/users',(req,res) =>{
+    const html =`
+    <ul>
+         ${users.map((user) => `<li>${user.first_name}</li>`).join("  ")}
+    </ul>
+    `
+    res.send(html)
 
-// });
+});
 
 //Rest Api will be started.....
 app.get('/api/users',(req,res) =>{
     return res.json(users)
 })
 
-// app.get('/api/users/:id',(req,res) =>{
-//     const id = Number(req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     return res.json(user);
-// })
+app.get('/api/users/:id',(req,res) =>{
+    const id = Number(req.params.id);
+    const user = users.find((user) => user.id === id);
+    return res.json(user);
+})
 
 app.route("api/users/:id").get((req,res) =>{
         const id = Number(req.params.id);
